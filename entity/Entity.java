954cf87355ed4e91fb363c26c786f314331df2a4
Ramell Collins <45057311+colliram@users.noreply.github.com>
2019-02-17 21:49:18 -0500

    Add files via upload

--- entity/Entity.java
+++ entity/Entity.java
@@ -1,5 +1,5 @@
 package entity;
-
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.GameMap;
@@ -8,7 +8,6 @@ public abstract class Entity {
     protected Vector2 pos;
     protected EntityType type;
     protected GameMap map;
-    protected boolean grounded;
 
     public Entity(float x, float y, EntityType type, GameMap map) {
         this.type = type;
@@ -22,46 +21,44 @@ public abstract class Entity {
 
     public abstract void render(SpriteBatch batch);
 
-    protected void moveX (float amount){
+    protected void moveX(float amount) {
         float newX = pos.x + amount;
-       // if (!map.doesRectCollideWithMap(newX, pos.y, getWidth(), getHeight())){
-            this.pos.x = newX;
-        //}
+        this.pos.x = newX;
     }
 
-    protected void moveY (float amount) {
+    protected void moveY(float amount) {
         float newY = pos.y + amount;
-       // if (!map.doesRectCollideWithMap(pos.x, newY, getWidth(), getHeight())) {
-            this.pos.y = newY;
-        //}
+        this.pos.y = newY;
     }
 
     public Vector2 getPos() {
         return pos;
     }
 
-    public float getX(){
+    public float getX() {
         return pos.x;
     }
 
-    public float getY(){
+    public float getY() {
         return pos.y;
     }
 
-    public EntityType getType(){
+    public EntityType getType() {
         return type;
     }
 
-   // public boolean isGrounded(){
-       // return grounded;
-  //  }
+    public Vector2 playerLocation(){
+        Vector2 loc = new Vector2();
+        loc.set(this.getX(), this.getY());
+        return loc;
+    }
 
-
-    public int getHeight(){
+    public int getHeight() {
         return type.getHeight();
     }
 
-    public int getWidth(){
+    public int getWidth() {
         return type.getHeight();
     }
 }
+
