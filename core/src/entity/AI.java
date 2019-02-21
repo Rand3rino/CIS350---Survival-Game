df84fad625c061073792ce3548921e81392e2326
Rand3rino <nguyeran@mail.gvsu.edu>
2019-02-20 21:12:22 -0500

    AI tracks the player and moves

--- core/src/entity/AI.java
+++ core/src/entity/AI.java
@@ -1,7 +1,5 @@
 package entity;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.GameMap;
@@ -12,15 +10,25 @@ public class AI extends Entity {
     private static final int speed = 1;
     Texture image;
     Vector2 vector = new Vector2();
-
-    public AI (float x, float y, GameMap map){
-        super(x,y,EntityType.COMPUTER, map);
+    Entity player;
+    
+    public AI (float x, float y, GameMap map, Entity e){
+        super(x,y,EntityType.COMPUTER, map, e);
+        player = e;
         image = new Texture ("C:\\Users\\Rand3\\Desktop\\CIS350\\core\\assets\\Battle_VM_single.png");
     }
 
-
     public void update (float deltaTime) {
 
+        vector = playerLocation();
+        if (pos.x  < vector.x)
+            moveX(speed);
+        else if (pos.x > vector.x)
+            moveX(-speed);
+        else if (pos.y < vector.y)
+            moveY(speed);
+        else if (pos.y > vector.y)
+            moveY(-speed);
 
         /** AI can move in a square **/
 //        if (pos.x  < 400 && pos.y == 300)
@@ -31,11 +39,21 @@ public class AI extends Entity {
 //            moveX(-speed);
 //        else
 //            moveY(-speed);
+
     }
 
+    // method "move to this point" Using tiledgamemap
+    public Vector2 playerLocation() {
+        vector.x = player.getX();
+        vector.y = player.getY();
+        System.out.println(vector.x + " " + vector.y);
+        return vector;
+
+    }
     @Override
     public void render(SpriteBatch batch) {
         batch.draw(image, pos.x, pos.y, getWidth(), getHeight());
     }
 
 }
+
