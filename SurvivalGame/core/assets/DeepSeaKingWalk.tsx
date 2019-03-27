2b5bc3b1fe2befad72dce1fff9d224f79ec5418c
Ramell Collins <45057311+colliram@users.noreply.github.com>
2019-03-27 09:06:32 -0400

    Delete DeepSeaKingWalk.tsx

--- SurvivalGame/core/assets/DeepSeaKingWalk.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="DeepSeaKingWalk" tilewidth="32" tileheight="32" tilecount="216" columns="18">
- <image source="DeepSeaKingWalk.png" width="576" height="384"/>
-</tileset>
