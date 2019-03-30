49f39ee0a42c8d1ce578bd918271eb9e9899d87a
Scott Nguyen <37782596+PoeticSenpai@users.noreply.github.com>
2019-03-29 21:16:22 -0400

    Add files via upload

---
+++ Logic/Point.java
@@ -0,0 +1,18 @@
+package Logic;
+
+public class Point {
+    private int xVal, yVal;
+    public Point(int x, int y){
+        xVal = x;
+        yVal = y;
+    }
+
+    public int getX(){
+        return xVal;
+    }
+
+    public int getY(){
+        return yVal;
+    }
+
+}
