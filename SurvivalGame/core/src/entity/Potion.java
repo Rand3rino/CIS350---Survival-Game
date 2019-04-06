e6c98e7b86fd80966ab5f417d46557d4b6ab27a1
Isfar Baset <43085165+IsfarBaset19@users.noreply.github.com>
2019-04-06 11:34:54 -0400

    potion entity

---
+++ SurvivalGame/core/src/entity/Potion.java
@@ -0,0 +1,72 @@
+package entity;
+
+
+import Logic.HealthTracking;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.mygdx.game.Collision;
+
+
+
+public class Potion  extends Entity {
+
+    TiledMapTileLayer collision;
+    Collision rect;
+
+    public HealthTracking health = new HealthTracking(this, null, 10, 10);
+
+    public Texture image;
+
+    Texture potion;
+
+    SpriteBatch batch;
+
+    /******************************************************************
+     * Potion Constructor
+     *
+     * @param x The potion's starting horizontal location
+     * @param y The potion's starting vertical location
+     * @param map The map that the potion is placed on
+     * @param e An entity object for entities to track
+     *****************************************************************/
+    public Potion (float x, float y, TiledMapTileLayer map, Entity e){
+
+        super(x,y,EntityType.POTION, map, e);
+        loadTextures();
+        image = potion;
+
+        this.rect = new Collision(getX(),getY(),getWidth(),getHeight());
+        this.collision = map;
+
+
+    }
+
+    private void loadTextures() {
+
+        potion = new Texture("PinkPotion.png");
+    }
+
+    public void render(SpriteBatch batch) {
+        batch.draw(image, pos.x, pos.y, getWidth(), getHeight());
+    }
+
+
+    // Checks if the player is colliding with the potion
+    public boolean PotionCollide() {
+        for(float step = 0; step < getHeight(); step += map.getTileHeight() / 2)
+            if(isPotion(getX() + getWidth(), getY() + step))
+                return true;
+        return false;
+    }
+
+    private boolean isPotion(float x, float y) {
+
+        TiledMapTileLayer.Cell cell = map.getCell((int) (x / map.getTileWidth()), (int) (y / map.getTileHeight()));
+        return cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("potion");
+    }
+
+
+
+
+}
