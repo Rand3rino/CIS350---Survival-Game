520cb88e4de757ff920b90525023fef8fbbae66e
Scott Nguyen <37782596+nguyensc@users.noreply.github.com>
2019-04-18 22:24:15 -0400

    HealthTracking Test Cases

--- SurvivalGame/core/src/Logic/HealthTrackingTest.java
+++ SurvivalGame/core/src/Logic/HealthTrackingTest.java
@@ -1,34 +1,90 @@
 package Logic;
 
-import entity.Entity;
-import entity.Potion;
+import entity.Player;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HealthTrackingTest {
 
-    private Entity e;
-
     @Test
-    void getHealth() {
+    public void constructorTest(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,3,3);
 
-
-            HealthTracking ht = new HealthTracking(e, 2, 1);
-
-            assertTrue(ht.getHealth() == 2);
-
+        assertEquals(3, h.getHealth());
     }
 
+    @Test
+    public void decHealthTest(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,3,3);
 
+        h.decreaseHealth(3);
+
+        assertEquals(0, h.getHealth());
+    }
 
     @Test
-    void isDead() {
+    public void decHealthTest2() {
+        Player p = new Player(100, 100, null, null);
+        HealthTracking h = new HealthTracking(p, 3, 3);
 
-        HealthTracking ht = new HealthTracking(e, 0, 1);
+        h.decreaseHealth(4);
 
-        assertTrue(ht.isDead());
-
+        assertEquals(0, h.getHealth());
     }
+
+    @Test
+    public void decHealthTest3(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,3,3);
+
+        h.decreaseHealth(2);
+
+        assertEquals(1,h.getHealth());
+    }
+
+    @Test
+    public void buffHealthTest(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,2,3);
+
+        h.buffHealth(1);
+
+        assertEquals(3,h.getHealth());
+    }
+
+    @Test
+    public void buffHealthTest2(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,1,3);
+        h.buffHealth(1);
+        assertEquals(2,h.getHealth());
+    }
+
+    @Test
+    public void buffHealthTest3(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,3,3);
+        h.buffHealth(1);
+
+        assertEquals(3,h.getHealth());
+    }
+
+    @Test
+    public void isDeadTest(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,3,3);
+        assertFalse(h.isDead());
+    }
+
+    @Test
+    public void isDeadTest2(){
+        Player p = new Player(100,100,null,null);
+        HealthTracking h = new HealthTracking(p,0,3);
+
+        assertTrue(h.isDead());
+    }
+
 }
